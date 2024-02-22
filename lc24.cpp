df288b93755ea9f76854f145f4e3ecfcc955fd36
Xiaoyun Wang <jayzx093123@126.com>
2024-02-21 21:35:58 -0500

    24. Swap Nodes in Pairs

---
+++ lc24.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+/**
+ * Definition for singly-linked list. */
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *swapPairs(ListNode *head)
+    {
+        ListNode *cur = new ListNode();
+        ListNode *dummyhead = new ListNode();
+        ListNode *temp1 = new ListNode();
+        ListNode *temp2 = new ListNode();
+        dummyhead->next = head;
+        cur = dummyhead;
+        if (head)
+        {
+            while (cur->next && cur->next->next)
+            {
+                temp1 = cur->next;
+                temp2 = cur->next->next->next;
+                cur->next = cur->next->next;
+                cur->next->next = temp1;
+                cur->next->next->next = temp2;
+                cur = cur->next->next;
+            }
+        }
+        return dummyhead->next;
+    }
+};
+
+// Function to print the linked list
+void printList(ListNode* head) {
+    while (head) {
+        std::cout << head->val << " ";
+        head = head->next;
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    // Create the input linked list: 1->2->3->4
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+
+    // Create an instance of the Solution class
+    Solution solution;
+
+    // Call the swapPairs method to swap the pairs of nodes
+    ListNode* swappedHead = solution.swapPairs(head);
+
+    // Print the output linked list after swapping pairs
+    std::cout << swappedHead;
+    
+    return 0;
+}
