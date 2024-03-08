0cc069d62d35fd0838dffb7f0ea322c16bf9d8f9
Xiaoyun Wang <jayzx093123@126.com>
2024-03-08 15:59:39 -0500

    Sorted Array

--- lc912.cpp
+++ lc912.cpp
@@ -3,22 +3,24 @@
 
 class Solution {
    public:
-    void function(int a, int b, std::vector<int>& nums, std::vector<int>& temp) {
+    void function(int a, int b, std::vector<int>& nums,
+                  std::vector<int>& temp) {
         if (b - a > 1) {
-            int m = b / 2;
+            int m = a + (b - a) / 2;
             function(a, m, nums, temp);
             function(m, b, nums, temp);
             int i = a;
             int j = m;
+            int count = i;
             while (i < m && j < b) {
-                if (temp[i] < temp[j]) {
-                    nums[i] = temp[i++];
-                } else {
-                    nums[i++] = temp[j++];
-                }
+                if (temp[i] < temp[j])
+                    nums[count++] = temp[i++];
+                else
+                    nums[count++] = temp[j++];
             }
-            while (i < m) nums[i] = temp[i++];
-            while (j < m) nums[i++] = temp[j++];
+            while (i < m) nums[count++] = temp[i++];
+            while (j < b) nums[count++] = temp[j++];
+            for (int k = a; k < b; k++) temp[k] = nums[k];
         }
     }
     std::vector<int> sortArray(std::vector<int>& nums) {
@@ -30,14 +32,14 @@ class Solution {
 };
 
 int main() {
-    std::vector<int> nums = {5, 2, 3, 1};
-    std::vector<int>& nums2 =
-        nums;  // nums2和nums是同个东西。引用实现了指针的功能
+    std::vector<int> nums = {5, 1, 1, 2, 0, 0};
+    // std::vector<int>& nums2 = nums;  //
+    // nums2和nums是同个东西。引用实现了指针的功能
     std::vector<int> ans;
     Solution solution;
     solution.sortArray(nums);
     for (int i = 0; i < nums.size(); i++) {
-        std::cout << nums[i++];
+        std::cout << nums[i];
     }
     return 0;
 }
